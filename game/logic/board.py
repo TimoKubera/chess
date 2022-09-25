6ed50cf191752909d33668400baa87c529facd9f
Timo <timokubera@yahoo.com>
2022-09-25 22:35:50 +0200

    Adding Piece, Board and Game classes

---
+++ game/logic/board.py
@@ -0,0 +1,15 @@
+X, Y = 0, 1
+A, B, C, D, E, F, G, H = 0, 1, 2, 3, 4, 5, 6, 7
+
+class Board():
+    """
+    Put description of the Board class here
+    """
+    def __init__(self):
+        # replace body with the initialization of a standard
+        # chess board with it's pieces placed correctly
+        pass
+
+    def print_board(self):
+        # replace body with how you want your board printed
+        pass
---
+++ game/logic/piece.py
@@ -0,0 +1,99 @@
+from operator import truediv
+from typing import List
+
+from board import *
+
+
+class Piece():
+    """
+    Put description of the Piece class here
+    """
+
+    def __init__(self, color: str, pos: List):
+        if color != "W" and color != "B":
+            raise Exception("Pieces can only be black, or white.")
+        
+        if len(pos) != 2 or A < pos[X] or H < pos[X] or 0 < pos[Y] or 7 < pos[Y]:
+            raise Exception("Pieces can't be placed here.")
+
+        self.pos = pos
+        self.color = color
+        
+
+    def getPos(self) -> List:
+        return self.pos
+    
+    def setPos(self, pos: List) -> None:
+        self.pos = pos
+        
+    def is_valid_move(self, target_pos: List) -> bool:
+        return False
+
+    def is_white(self) -> bool:
+        if self.color == "W":
+            return True
+
+        return False
+
+    def __str__(self):
+        return ""
+        
+# I'll add which parameters I generally used for the specific subclasses
+# in the following Rook class, but note you may need more or less depending
+# on your specific implementation
+class Rook(Piece):
+    def __init__(self, color, pos):
+        super().__init__(color, pos) 
+        # parrent init function to avoid the same lines of code
+        pass
+
+    def is_valid_move(self, board, start, to):
+        pass
+
+class Knight(Piece):
+    def __init__(self):
+        pass
+
+    def is_valid_move(self):
+        pass
+
+class Bishop(Piece):
+    def __init__(self):
+        pass
+
+    def is_valid_move(self):
+        pass
+
+class Queen(Piece):
+    def __init__(self):
+        pass
+
+    def is_valid_move(self):
+        pass
+
+class King(Piece):
+    def __init__(self):
+        pass
+
+    def is_valid_move(self):
+        pass
+    
+    # I added an extra method for the King class
+    def can_castle(self):
+        pass
+
+# Class to represent a pseudo pawn that can be taken when
+# en passant is possible
+class GhostPawn(Piece):
+    def __init__(self, color):
+        pass
+
+    def is_valid_move(self):
+        return False
+
+class Pawn(Piece):
+    def __init__(self):
+        pass
+
+    def is_valid_move(self):
+        pass
